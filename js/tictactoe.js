db28f50990c2c6153018ac921fd1033a60fb9ccc
Jin <jin@example>
2017-02-28 12:30:42 +1100

    add working files

---
+++ js/tictactoe.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+// create grid value
+// game start - board display blank
+// when user click - write value in cell
+// check cell id pattern 3x3 === winning combination or coordinates
+// iterate 3 attemps, set score counter for winning if X -, winning score = //  3,
+//
+// if select the non-winning cell - no score,
+//
+//function count() {
+// 	if(x<1)}
+//    x += 1;}
+// 	else {
+// 		x = 0;
+// 	}
+// }
+// check the pattern
+// set default null vs blank
+
+    var board = $(".cell");
+    var cross = x;
+    var nought = o;
+    var gameStart = "_";
+
+    $(".cell").on("click", function(){
+      alert(.this).$("#cell")
+    };
+
+    var placeX = function(chooseCell){
+      var xWin = [[r1_1, r1_2, r1_3],
+                  [r2_1, r2_2, r2_3],
+                  [r3_1, r3_5, r3_9],
+                  [r1_1],r2_2, r3_9],
+                  [r1_1, r2_1, r3_1]];
+
+    for (var i = 0; i < 3; i++) {
+      placeX[i]};
+
+      if {chooseCell =
+
+
+
+  }
+
+  // $(".cell").on("click", function(){
+  //  (.this).append('cross').($("#cell"))};
+};
