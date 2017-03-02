3e82999764feef3a9b9a8c90983036900f6186ae
Jin <jin@example>
2017-03-03 10:02:53 +1100

    game_works

---
+++ README.md
@@ -0,0 +1,25 @@
+Project: Two Players Tic-Tac-Toe Game
+Demo: url here
+Development tools:
+	•	Javascript
+	•	JQuery
+	•	HTML5
+	•	CSS
+
+About the game:  
+This is a Javascript based game that is played via  a web browser. The game is designed for two players on a 3x3 grid game board.
+
+Target audience is children aged 3-9 years.
+
+First player represented by X in the script and a gingerbread man visually in the game.
+Second player represented by O in the script and a donut visually in the game.
+
+How the game works:
+On start, first player (X) selects and clicks in a cell which will be marked by gingerbread man image. Next turn change for second player (O) to select and click in a cell which will be marked by donut image .
+When a cell is clicked and the image is placed, that cell becomes inactive and cannot be selected again by either player.
+This continues until all cells are filled or a player gets three of their images in a row (horizontal, vertical or diagonal).
+The player who placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+If there is no winning pattern selected, it is a draw game.
+
+Result messages will display and hold for 4 seconds for the game to restart, or the player can click the Game Start button to refresh the page starting the new game.
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
 
-//var cnt = 0;
-          // for (var i = 9; i < board.length; i++) {
-          //   board[i]
-          // }
-          //   return "draw";  // Cat's game!
-          // }
-
 
     // var board = $(".cell");
 
-    var player1 = 'x';
-    var player2 = 'o';
+    var player1 = "../images/man.png";
+    var player2 = "../images/donut.png";
     var winner = "";
     var turnCount = 0;
+    var $msg = $('#message'); //create $msg to store result messages
+    //set currentTurn
+    var currentTurn = 'x'; //set the starting turn in this case set as 'x'to have first turn
 
-
-    var currentTurn = 'x';
+    var gameOver = false;  // set the ininial game start
 
     window.board = [
       ["","",""],
@@ -24,8 +19,7 @@ $(document).ready(function() {
       ["","",""]
     ];
 
-
-
+// create check x winning function, and define winning pattern
     var checkWin_x = function () {
       if( (board[0][0] + board[0][1] + board[0][2]) === 'xxx'){
         return true; // first row
@@ -57,11 +51,10 @@ $(document).ready(function() {
         return true; //diagonal works
       }
 
-      console.log("Play again");
       return false;
 
     };
-
+//create check winning function and define winning pattern for '0'
     var checkWin_o = function () {
 
       if( (board[0][0] + board[0][1] + board[0][2]) === 'ooo'){
@@ -89,12 +82,11 @@ $(document).ready(function() {
       if ( (board[0][0] + board[1][1] + board[2][2]) === 'ooo'){
         return true; // diagonal ok
       }
-      if ( (board[2][0] + board[1][1] + board[2][0]) === 'ooo'){
+      if ( (board[2][0] + board[1][1] + board[0][2]) === 'ooo'){
         return true; // diagonal -45 ok
 
       }
 
-      console.log("Play again");
       return false;
 
   }; // end of checkWin_o
@@ -103,28 +95,43 @@ $(document).ready(function() {
     var cellClick = function(){
       console.log('clicked!');
 
+      var id = this.id;
+      var row = id[0];
+      var col = id[1];
+
+      // Return early from this click handler function (which means we ignore the click)
+      // if the game is over, or the square is already taken (not empty)
+      if( gameOver || board[row][col] !== "" ){
+        return;
+      }
+
       turnCount++;
       console.log('count:', turnCount);
       // turnCount += 1;
       // turnCount = turnCount + 1;
 
-      var id = this.id;
-      var row = id[0];
-      var col = id[1];
       board[row][col] = currentTurn;
       console.log(this);
-      $(this).css('background-image', 'url(../images/man.png)');
-      $(this).html( currentTurn );
+
+      // $(this).css('background-image', 'url(images/man.png)');
+
+      $(this).addClass( currentTurn );
+
+      // $(this).html( currentTurn );
 
       // var xWin = checkWin_x();
       if( checkWin_x() ){
-        alert('X wins!');
+        $msg.html('X, You win!');
+        // alert('X wins!');
+        gameOver = true;
         gameReset();
       } else if( checkWin_o() ){
-        alert('O wins!');
+        $msg.html('O, You win!');
+        gameOver = true;
         gameReset();
       } else if ( turnCount === 9 ) {
-        alert ("It's a draw");
+        $msg.html("It's a draw!");
+        gameOver = true;
         gameReset();
       }
 
@@ -137,20 +144,27 @@ $(document).ready(function() {
       }
 
     };
-
-    var gameReset = function(){
+//create gameReset function to clear the board
+    window.gameReset = function(){
+      console.log("Game reset running");
       window.board = [
         ["","",""],
         ["","",""],
         ["","",""]
       ]
-      $(".cell").html("");
-      turnCount === 0;
-    }
+//set time out for game result message display - 3s, clear message, clear board
+      setTimeout(function(){
+        $(".cell").removeClass('x').removeClass('o');
+        turnCount = 0;
+        $msg.html('');
+        gameOver = false;
+      }, 4000);
 
-    // $(".cell").on("click", cellClick);
+    };
 
-    $(".cell").on("click", cellClick);
+    $("#start").on("click", gameReset); //set gameReset function to event handler
+
+    $(".cell").on("click", cellClick); // create eventhandler click cell
 
 
 
